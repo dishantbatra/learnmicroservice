9e2746cd6b9c866435d101d5faeabc41193c158f
Dishant Batra <Dishant.Batra@1e.com>
2023-09-23 09:17:50 +0530

    Dicount related changes

---
+++ src/Services/Discount/Discount.API/Program.cs
@@ -0,0 +1,23 @@
+using Discount.API.Repositories;
+using Discount.API.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
+
+}
+
+app.MapControllers();
+app.Run();
